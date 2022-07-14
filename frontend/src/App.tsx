e6c971723e099dd553e9e0c79e3a7d458d16f9a6
Richard Ribeiro <richardpenido@live.com>
2022-07-14 20:27:36 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,13 @@
+import NotificationButton from "./components/Notification-Button"
+
 function App() {
-  return(
-    <h1>Olá mundo!</h1>
+  return (
+    <>
+      <h1>Olá mundo!</h1>
+      <NotificationButton />
+    </>
   )
-  
+
 }
 
 export default App
